040b9d0e56b8c99006b05431d2b828d07a851b4d
MOHO桑 <iam880401@gmail.com>
2021-06-25 12:35:54 +0800

    make it better

--- song/views.py
+++ song/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import song.models as models
-from song.models import Song
 from song.utils import analyzeTitle, getfromList
 from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from django.contrib import messages
@@ -21,7 +20,7 @@ def addsong(request):
                     data = analyzeTitle(song)
                     name = data[1] if len(data) > 1 else data[0]
                     artist = data[0] if len(data) > 1 else 'Unknown'
-                    Song.objects.savesong(name=name, artist=artist)
+                    models.Song.objects.savesong(name=name, artist=artist)
                     data.clear()
                 messages.success(request, 'Success')
                 return HttpResponseRedirect('/')
